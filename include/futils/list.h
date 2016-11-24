fb568dc8d450ae231a3d922bc7f4b98f9c5fe97f
Romain Roffé <romain.roffe@parrot.com>
2016-11-24 18:37:55 +0100

    [DEV] Use __typeof__ instead of typeof
    Change-Id: Id652b9ca8b76a9bd5818e3227970e6159fdd956f
    Reviewed-on: https://gerrit.parrot.biz/57019
    Reviewed-by: Jean-Baptiste Dubois <jean-baptiste.dubois@parrot.com>

--- include/futils/list.h
+++ include/futils/list.h
@@ -52,7 +52,7 @@ extern "C" {
  **/
 #define container_of(ptr, type, member)				\
 ({									\
-	const typeof(((type *)0)->member) * __mptr = (ptr);		\
+	const __typeof__(((type *)0)->member) * __mptr = (ptr);		\
 	(type *)((uintptr_t)__mptr - offsetof(type, member));	\
 })
 
